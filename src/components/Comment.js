0605ebbc5c61b8dd5327ad53800403ff4ba124e4
Rahul Khattri <rahulkhattri14@gmail.com>
2023-12-12 13:06:02 +0530

    added n level nested comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,18 +1,65 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { FaUserCircle } from "react-icons/fa";
 
 
-const Comment = ({ data }) => {
+const Comment = ({ data,setComments,comments,root_id }) => {
+  console.log(comments)
+  function getUpdatedComments(root_id,comments,text,id){
+    const updatedComments = comments.map((rootComment)=>{
+      if(root_id!==rootComment.root_id) return rootComment
+      else{
+        return addReply(rootComment,id,text)
+      }
+    })
+    return updatedComments
+  }
+  function addReply(comment,id,text){
+    if(comment.id===id){
+      return {
+        ...comment,
+        replies : [
+          ...comment.replies,
+          {
+            id : Date.now(),
+            name: "Rahul Khattri",
+            text : text,
+            replies : []
+          }
+        ]
+      }
+    }
+    const updatedReplies = comment.replies.map((reply)=>addReply(reply,id,text))
+    return {...comment , replies : updatedReplies}
+  }
+  const [showReplyInput,setShowReplyInput] = useState(false)
+  const replyRef = useRef()
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-row items-center gap-x-1">
-        <p>{data.name}</p>
+    <div className="flex flex-col gap-y-4">
+      <div className="flex flex-row items-center gap-x-4">
+        <div className="flex flex-row gap-x-2 items-center"> 
+          <FaUserCircle className="text-2xl"/>
+          <p>{data.name}</p>
+        </div>
         <p>{data.text}</p>
+        <p className="text-md text-blue-500 ml-6 cursor-pointer" onClick={()=>setShowReplyInput(true)}>Reply</p>
       </div>
       { data.replies.length > 0 && <div className="flex flex-col gap-y-2 p-8 border-l-2 border-black">
         {data.replies.map((reply) => {
-          return <Comment data={reply} />;
+          return <Comment root_id={root_id} key={reply.id} data={reply} setComments={setComments} comments={comments} />;
         })}
       </div>}
+      {showReplyInput && <form className="flex flex-row gap-x-2" onSubmit={(e)=>{
+        e.preventDefault()
+        const text = replyRef.current.value
+        if(text){
+          const updatedComments = getUpdatedComments(root_id,comments,text,data.id)
+          setComments(updatedComments)
+        }
+        setShowReplyInput(false)
+      }}>
+        <input type="text" className="w-[90%] rounded-md" placeholder="Add Reply..." autoFocus ref={replyRef} onBlur={()=>setShowReplyInput(false)}/>
+        <button className="w-[10%] bg-green-400 rounded-md" type="submit">Add</button>
+      </form>}
     </div>
   );
 };
--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,16 +1,37 @@
-import React from "react";
+import { useRef, useState } from "react";
 import Comment from "./Comment";
-import { comments } from "../data/commentData";
+// import { comments } from "../data/commentData";
 const CommentBox = () => {
+  const [comments,setComments] = useState([])
+  const inputRef = useRef(null)
   return (
     <div className="m-4 flex flex-col gap-y-2">
         <p className="text-black font-bold text-2xl">Comments:</p>
-        <div className="bg-slate-400 p-2 rounded-lg">
+        { comments.length === 0 ? <p className="text-black text-md">No Comments!</p> : <div className="p-2 flex flex-col gap-y-4 bg-slate-300 rounded-lg">
             {comments.map((c) => (
-                <Comment data={c} />
+                <Comment  root_id={c.root_id} key={c.id} data={c} comments={comments} setComments={setComments}/>
             ))}
-        </div>
-      
+        </div>}
+        <form className="flex flex-row gap-x-2 p-4" onSubmit={(e)=>{
+          e.preventDefault()
+          const comment = inputRef.current.value
+          if(comment){
+            setComments([
+              ...comments,
+              {
+                root_id : comments.length,
+                id : Date.now(),
+                name : "Rahul Khattri",
+                text : comment,
+                replies : []
+              }
+            ])
+          }
+          inputRef.current.value = ""
+        }}>
+            <input type="text" placeholder="Add Comment..." className="w-[90%]" ref={inputRef}/>
+            <button className="rounded-md bg-green-400 text-white p-1 w-[10%]" type="submit">Add</button>
+        </form>
     </div>
   );
 };
